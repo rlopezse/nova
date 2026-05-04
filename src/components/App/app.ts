1c5ca73975f268c1b39f5496ca7ab0fd750c7c93
rlopezse <rza.lopez.s@gmail.com>
2026-05-03 22:33:45 -0400

    added: algunas cosas

--- src/components/App/app.ts
+++ src/components/App/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FilterComponent } from '../Filter/filter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FilterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/components/Filter/filter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.html',
+  styleUrl: './filter.scss',
+})
+
+export class FilterComponent {}
