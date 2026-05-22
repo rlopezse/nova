d2a5fe30837ac6600ecf74583191d2e2f4d1012d
rlopezse <rza.lopez.s@gmail.com>
2026-05-22 10:07:47 -0400

    updated: migracion a pnpm

--- src/components/filter/filter.ts
+++ src/components/filter/filter.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 //import { FilterService } from '../../services/filter/filter.service';
 
-
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.html',
   styleUrl: './filter.scss',
 })
 export class FilterComponent {
-  categories: string[] = ['apple', 'samsung', 'xiaomi', 'huawei']
-  ordenarPor: string[] = ['menor precio', 'mayor precio']
+  categories: string[] = ['apple', 'samsung', 'xiaomi', 'huawei'];
+  ordenarPor: string[] = ['menor precio', 'mayor precio'];
   //filterService = inject(FilterService);
 }
