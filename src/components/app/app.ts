7fa59c5dbb4a7b5a720210dd64ab75c43ad5a8b2
rlopezse <rza.lopez.s@gmail.com>
2026-05-21 22:20:39 -0400

    fix: rename files to lowercase

---
+++ src/components/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar';
+
+@Component({
+  selector: 'app-component',
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class AppComponent {}
---
+++ src/components/filter/filter.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//import { FilterService } from '../../services/filter/filter.service';
+
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.html',
+  styleUrl: './filter.scss',
+})
+export class FilterComponent {
+  categories: string[] = ['apple', 'samsung', 'xiaomi', 'huawei']
+  ordenarPor: string[] = ['menor precio', 'mayor precio']
+  //filterService = inject(FilterService);
+}
